b7f1c87135af2e6b9be3c7bc11f64e3817447b07
Brandon Torres <brandon09925.batb@gmail.com>
2022-04-15 20:49:19 -0500

    Sección de pruebas: Clase 58

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+//Antes
+/**
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+import './index.css';
+// const saludo = <h1>Hola Mundo</h1>;
+const divRoot = document.querySelector('#app');
+ReactDOM.render(<CounterApp value={10}/>, divRoot);
+ */
+
+//React +18
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 // import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 import './index.css';
 
 // const saludo = <h1>Hola Mundo</h1>;
 const divRoot = document.querySelector('#app');
-ReactDOM.render(<CounterApp value={10}/>, divRoot);
+const root = createRoot(divRoot);
+root.render(<CounterApp value={10}/>);
